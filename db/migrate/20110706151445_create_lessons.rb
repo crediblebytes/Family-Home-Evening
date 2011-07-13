82c9c8de0d40501df2a07fd84f9836db82e50353
Don Page <goodies4uall@gmail.com>
2011-07-13 03:01:21 -0600

    Fixed character type inside database to be text not string

--- db/migrate/20110706151445_create_lessons.rb
+++ db/migrate/20110706151445_create_lessons.rb
@@ -2,7 +2,7 @@ class CreateLessons < ActiveRecord::Migration
   def self.up
     create_table :lessons do |t|
       t.string :title
-      t.string :content
+      t.text :content
       t.integer :user_id
 
       t.timestamps
