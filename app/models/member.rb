20da6423ac7c971fa4a6467ff0e4a2ab0b763b0f
Don Page <goodies4uall@gmail.com>
2011-07-18 19:24:55 -0600

    paperclip

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,7 +2,15 @@ class Member < ActiveRecord::Base
 	attr_accessible :name, :birthdate, :email, :pic
 
 	belongs_to :user
-   has_attached_file :pic
+   has_attached_file :pic,
+                     :storage => :s3,
+                     :bucket => 'fhe-app',
+                     :path => "/members/pic/:id/:style/:filename",
+                     :s3_credentials => {
+                        :access_key_id => ENV['S3_KEY'] || 'AKIAIG7I3ZRV4WWYCWAA',
+                        :secret_access_key => ENV['S3_SECRET'] || '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'
+                     }
+
 
 
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
