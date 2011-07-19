0e30b06d44b528d6a80655f5144b6e9afac06fb5
Don Page <goodies4uall@gmail.com>
2011-07-18 19:01:31 -0600

    paperclip

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,10 +2,8 @@ class Member < ActiveRecord::Base
 	attr_accessible :name, :birthdate, :email, :pic
 
 	belongs_to :user
-   has_attached_file :pic,
-                     :styles => {
-                        :thumb=> "100x100#",
-                        :small  => "400x400>" }
+   has_attached_file :pic
+
 
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
